ac1c6d724305864030a514d643660c7692086bbd
sci3nti5t <bradleytannor@gmail.com>
2022-11-22 13:05:54 -0800

    Update test_data_type.rb
    Substituted equivalent strings and string literals.

--- test/test_data_type.rb
+++ test/test_data_type.rb
@@ -15,41 +15,41 @@ class TestDataType < Minitest::Test
   CLASS_NUMERIC = Numeric
 
   def test_git_paths()
-    assert_path_exists(".github/workflows/ruby.yml")
-    assert_path_exists(".gitignore")
+    assert_path_exists('.github/workflows/ruby.yml')
+    assert_path_exists('.gitignore')
   end
 
   def test_doc_paths()
 
-    assert_path_exists("LICENSE.txt")
-    assert_path_exists("README.md")
-    assert_path_exists("CODE_OF_CONDUCT.md")
-    refute_path_exists(".yardoc") # Fail locally, pass remotely
-    assert_path_exists("Changelog.md")
+    assert_path_exists('LICENSE.txt')
+    assert_path_exists('README.md')
+    assert_path_exists('CODE_OF_CONDUCT.md')
+    refute_path_exists('.yardoc') # Fail locally, pass remotely
+    assert_path_exists('CHANGELOG.md')
 
   end
 
   def test_gem_paths()
 
-    assert_path_exists("Rakefile")
-    assert_path_exists("Gemfile")
-    assert_path_exists("Gemfile.lock")
+    assert_path_exists('Rakefile')
+    assert_path_exists('Gemfile')
+    assert_path_exists('Gemfile.lock')
 
   end
 
   def test_test_paths()
-    assert_path_exists("test/test_data_type.rb")
-    assert_path_exists("test/test_helper.rb")
+    assert_path_exists('test/test_data_type.rb')
+    assert_path_exists('test/test_helper.rb')
   end
 
   def test_lib_paths()
-    assert_path_exists("lib/data_type.rb")
-    assert_path_exists("lib/data_type/version.rb")
+    assert_path_exists('lib/data_type.rb')
+    assert_path_exists('lib/data_type/version.rb')
   end
 
   def test_bin_paths()
-    assert_path_exists("bin/console")
-    assert_path_exists("bin/setup")
+    assert_path_exists('bin/console')
+    assert_path_exists('bin/setup')
   end
 
   def test_that_it_has_a_version_number() refute_nil(CLASS::VERSION) end
